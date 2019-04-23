ff08312f2af6350bbe6921c3de8e2773abec47c8
AnteVuletic <antevuletic1995@gmail.com>
2019-04-23 05:34:01 +0200

    JSON-Server seten up, basic component layout and basic routing completed

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default _ =>
+<div>
+    <Link to="/dogs">Dogs</Link>
+    <Link to="/cats">Cats</Link>
+</div>
---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './NotFound.css';
+
+export default _ => <div class="NotFound">404 Page not found.</div>
